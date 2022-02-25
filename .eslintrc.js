184f291274db8113cab8c5a2b1c7f0d61e8a680a
David Wang <dwang733@outlook.com>
2022-02-25 06:21:51 -0800

    Add linters and use hooks

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended"],
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"]
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,62 @@
-import React, { createRef } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+import logo from "./logo.svg";
 import { Timeline } from "vis-timeline";
 import { DataSet } from "vis-data";
 
-class TimeLineComponent extends React.Component <{}> {
+function TimelineComponent() {
+  const items = useRef(
+    new DataSet([
+      { id: 1, content: "item 1", start: "2013-04-20" },
+      { id: 2, content: "item 2", start: "2013-04-14" },
+      { id: 3, content: "item 3", start: "2013-04-18" },
+      { id: 4, content: "item 4", start: "2013-04-16", end: "2013-04-19" },
+      { id: 5, content: "item 5", start: "2013-04-25" },
+      { id: 6, content: "item 6", start: "2013-04-27" }
+    ])
+  );
+  // Configuration for the Timeline
+  const options = useRef({});
 
-  private timelineDiv = createRef<HTMLDivElement>()
+  const timelineDivRef = useRef<HTMLDivElement>(null);
+  const timeline = useRef<Timeline | null>(null);
+  useEffect(
+    () => {
+      if (timeline.current == null) {
+        timeline.current =
+          timelineDivRef.current &&
+          new Timeline(timelineDivRef.current, items.current, options.current);
+      }
+    },
+    [timelineDivRef, items, options]
+  );
 
-  componentDidMount() {
-    // Create a DataSet (allows two way data-binding)
-    var items = new DataSet([
-      {id: 1, content: 'item 1', start: '2013-04-20'},
-      {id: 2, content: 'item 2', start: '2013-04-14'},
-      {id: 3, content: 'item 3', start: '2013-04-18'},
-      {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'},
-      {id: 5, content: 'item 5', start: '2013-04-25'},
-      {id: 6, content: 'item 6', start: '2013-04-27'}
-    ]);
-
-    // Configuration for the Timeline
-    var options = {};
-
-    // Create a Timeline
-    var timeline = this.timelineDiv.current && new Timeline(this.timelineDiv.current, items, options);
-  }
-
-  render() {
-    return (
-      <div id="visualization" ref={this.timelineDiv}></div>
-    )
-  }
+  return <div id="visualization" ref={timelineDivRef} />;
 }
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-        <script type="text/javascript" src="https://unpkg.com/moment@latest"></script>
-        <script type="text/javascript" src="https://unpkg.com/vis-data@latest/peer/umd/vis-data.min.js"></script>
-        <script type="text/javascript" src="https://unpkg.com/vis-timeline@latest/peer/umd/vis-timeline-graph2d.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="https://unpkg.com/vis-timeline/styles/vis-timeline-graph2d.min.css" />
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <script type="text/javascript" src="https://unpkg.com/moment@latest" />
+        <script
+          type="text/javascript"
+          src="https://unpkg.com/vis-data@latest/peer/umd/vis-data.min.js"
+        />
+        <script
+          type="text/javascript"
+          src="https://unpkg.com/vis-timeline@latest/peer/umd/vis-timeline-graph2d.min.js"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://unpkg.com/vis-timeline/styles/vis-timeline-graph2d.min.css"
+        />
       </header>
 
-      <TimeLineComponent />
+      <TimelineComponent />
     </div>
-  );  
+  );
 }
 
 export default App;
