f929416584a5e85c0a0fbec95dbdf5fb08026572
David Wang <dwang733@outlook.com>
2022-02-26 21:36:15 -0800

    Re-enable moment and sort imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { DataSet } from 'vis-data/peer';
-import { Timeline } from 'vis-timeline/peer';
 import 'vis-timeline/styles/vis-timeline-graph2d.min.css';
+import './App.css';
 
 import React, { useEffect, useRef } from 'react';
-import './App.css';
-// import moment from 'moment/moment';
+
+import { DataSet } from 'vis-data/peer';
+import { Timeline } from 'vis-timeline/peer';
+import moment from 'moment/moment';
 
 function TimelineComponent() {
   const items = useRef(
@@ -12,34 +13,28 @@ function TimelineComponent() {
       { id: 1, content: 'item 1', start: 0 },
       { id: 2, content: 'item 2', start: 1000 },
       { id: 3, content: 'item 3', start: 2000 },
-      {
-        id: 4,
-        content: 'item 4',
-        start: 3000,
-        end: 5000,
-      },
+      { id: 4, content: 'item 4', start: 3000, end: 5000 },
       { id: 5, content: 'item 5', start: 4000 },
       { id: 6, content: 'item 6', start: 5000 },
     ])
   );
   // // Configuration for the Timeline
-  // const options = useRef({
-  //   start: -5000,
-  //   end: 60000,
-  //   min: -30000, // -30 sec
-  //   max: 1800000, // 30 min
-  //   format: {
-  //     majorLabels: {
-  //       second: 'm[m]',
-  //     },
-  //   },
-  //   zoomMin: 10000,
-  //   zoomMax: 600000,
-  //   moment(date: moment.MomentInput) {
-  //     return moment(date).utc();
-  //   },
-  // });
-  const options = useRef({});
+  const options = useRef({
+    start: -5000,
+    end: 60000,
+    min: -30000, // -30 sec
+    max: 1800000, // 30 min
+    format: {
+      majorLabels: {
+        second: 'm[m]',
+      },
+    },
+    zoomMin: 10000,
+    zoomMax: 600000,
+    moment(date: moment.MomentInput) {
+      return moment(date).utc();
+    },
+  });
 
   const timelineDivRef = useRef<HTMLDivElement>(null);
   const timeline = useRef<Timeline | null>(null);
