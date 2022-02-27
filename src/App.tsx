531d0640a99860220511cb412fbd94fccde72baf
David Wang <dwang733@outlook.com>
2022-02-26 21:22:05 -0800

    Switch to Prettier format > eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,59 @@
-import "vis-data/peer";
-import "vis-timeline/peer";
-import "vis-timeline/styles/vis-timeline-graph2d.min.css";
+import { DataSet } from 'vis-data/peer';
+import { Timeline } from 'vis-timeline/peer';
+import 'vis-timeline/styles/vis-timeline-graph2d.min.css';
 
-import React, { useEffect, useRef } from "react";
-import "./App.css";
-import moment from "moment/moment";
-import { Timeline } from "vis-timeline/peer";
-import { DataSet } from "vis-data/peer";
+import React, { useEffect, useRef } from 'react';
+import './App.css';
+// import moment from 'moment/moment';
 
 function TimelineComponent() {
   const items = useRef(
     new DataSet([
-      { id: 1, content: "item 1", start: 0 },
-      { id: 2, content: "item 2", start: 1000 },
-      { id: 3, content: "item 3", start: 2000 },
-      { id: 4, content: "item 4", start: 3000, end: 5000 },
-      { id: 5, content: "item 5", start: 4000 },
-      { id: 6, content: "item 6", start: 5000 }
+      { id: 1, content: 'item 1', start: 0 },
+      { id: 2, content: 'item 2', start: 1000 },
+      { id: 3, content: 'item 3', start: 2000 },
+      {
+        id: 4,
+        content: 'item 4',
+        start: 3000,
+        end: 5000,
+      },
+      { id: 5, content: 'item 5', start: 4000 },
+      { id: 6, content: 'item 6', start: 5000 },
     ])
   );
   // // Configuration for the Timeline
-  const options = useRef({
-    start: -5000,
-    end: 60000,
-    min: -30000, // -30 sec
-    max: 1800000, // 30 min
-    format: {
-      majorLabels: {
-        second: "m[m]"
-      }
-    },
-    zoomMin: 10000,
-    zoomMax: 600000,
-    moment: function(date: moment.MomentInput) {
-      return moment(date).utc();
-    }
-  });
+  // const options = useRef({
+  //   start: -5000,
+  //   end: 60000,
+  //   min: -30000, // -30 sec
+  //   max: 1800000, // 30 min
+  //   format: {
+  //     majorLabels: {
+  //       second: 'm[m]',
+  //     },
+  //   },
+  //   zoomMin: 10000,
+  //   zoomMax: 600000,
+  //   moment(date: moment.MomentInput) {
+  //     return moment(date).utc();
+  //   },
+  // });
+  const options = useRef({});
 
   const timelineDivRef = useRef<HTMLDivElement>(null);
   const timeline = useRef<Timeline | null>(null);
-  useEffect(
-    () => {
-      if (timeline.current == null) {
-        timeline.current =
-          timelineDivRef.current &&
-          new Timeline(timelineDivRef.current, items.current, options.current);
-      }
+  useEffect(() => {
+    if (timeline.current == null) {
+      timeline.current =
+        timelineDivRef.current &&
+        new Timeline(timelineDivRef.current, items.current, options.current);
+    }
 
-      // timeline.current =
-      //   timelineDivRef.current &&
-      //   new Timeline(timelineDivRef.current, items.current, options.current);
-    },
-    [timelineDivRef, items, options]
-  );
+    // timeline.current =
+    //   timelineDivRef.current &&
+    //   new Timeline(timelineDivRef.current, items.current, options.current);
+  }, [timelineDivRef, items, options]);
 
   return <div id="visualization" ref={timelineDivRef} />;
 }
